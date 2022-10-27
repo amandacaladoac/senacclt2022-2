28630e26ff784104fe64732db008d6c147c6cd36
Amanda <amandabcalado@icloud.com>
2022-10-27 09:16:56 -0300

    java

---
+++ AulaJAVA/Entidades/Agencia.java
@@ -0,0 +1,38 @@
+package AulaJAVA.Entidades;
+
+public class Agencia {
+    private int numero;
+    private String telefone;
+    private String email;
+    private Endereco endereco;
+
+    public Agencia() {
+    }
+
+    public Agencia(int numero, String telefone, String email, Endereco end) {
+        this.numero = numero;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ AulaJAVA/Entidades/Endereco.java
@@ -0,0 +1,16 @@
+package AulaJAVA.entidades;
+
+public class Endereco {
+    private String uf;
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private int numero;
+    private String comp;
+    private String cep;
+
+    public Endereco() {
+    }s
+
+
+}
